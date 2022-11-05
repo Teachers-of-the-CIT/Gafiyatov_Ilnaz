0a0996a61dadf434f0c4febb0d0e85107e6da786
Ilnaz <gafiyatov.guf@gmail.com>
2022-11-05 11:56:51 +0300

    final commit

---
+++ demo/demo/UserPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Interaction logic for UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Window
+    {
+        string name;
+
+        public UserPage(string fullname)
+        {
+            InitializeComponent();
+            FullName.Text = fullname;
+            name = fullname;
+        }
+
+        private void LogoutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow page = new MainWindow();
+            this.Close();
+            page.Show();
+        }
+
+        private void OrdersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UserProducts page = new UserProducts(name);
+            this.Close();
+            page.Show();
+        }
+
+        private void ProductsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UserProducts page = new UserProducts(name);
+            this.Close();
+            page.Show();
+        }
+    }
+}
